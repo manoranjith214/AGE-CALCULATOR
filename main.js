a715843c1f218cd3bb57e894421fbf63a7f11a33
manoranjith214 <ranjithmano2006@gmail.com>
2024-11-23 15:06:01 +0530

    AGE CALCULATOR

--- main.js
+++ main.js
@@ -2,19 +2,33 @@ function calculateAge() {
     const ageInput = document.getElementById("age").value;
     const resultDiv = document.getElementById("age-result");
 
+    if (!ageInput) {
+        resultDiv.innerHTML = "Please enter a valid date.";
+        return;
+    }
 
     const ageDate = new Date(ageInput);
     const today = new Date();
+
+    if (isNaN(ageDate)) {
+        resultDiv.innerHTML = "Invalid date format.";
+        return;
+    }
+
     let age = today.getFullYear() - ageDate.getFullYear();
     const monthDifference = today.getMonth() - ageDate.getMonth();
-    
+    const dayDifference = today.getDate() - ageDate.getDate();
 
-    resultDiv.innerHTML = `${age}`;
+   
+    if (monthDifference < 0 || (monthDifference === 0 && dayDifference < 0)) {
+        age--;
+    }
+
+    resultDiv.innerHTML = ` ${age}`;
 }
 
 const btn = document.getElementById("btn");
-
-if(btn.addEventListener("click", calculateAge));
+btn.addEventListener("click", calculateAge);
 
 
 
